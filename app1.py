e0d625212e26fd41a8b09084be83ce6246b43fd2
H574949 <54100056+H574949@users.noreply.github.com>
2021-11-12 23:35:05 +0100

    Add files via upload

--- app1.py
+++ app1.py
@@ -10,7 +10,7 @@ import os
 class StreamlitApp:
 
     def __init__(self):
-        ppath = Path(__file__).parents[1] / 'ml2tmdb_03/box_office.pkl'
+        ppath = Path(__file__).parents[1] / 'ml2tmdb_03/box_office'
         self.model = load_model(ppath) 
         self.save_fn = 'path.csv'
 
